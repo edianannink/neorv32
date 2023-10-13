9a9d0c7bf13300b506ae6b914f01477e2b87adee
edianannink <e_dian@live.nl>
2023-10-13 16:01:54 +0200

    Instruction memory on eNVM

--- rtl/processor_templates/neorv32_ProcessorTop_Minimal.vhd
+++ rtl/processor_templates/neorv32_ProcessorTop_Minimal.vhd
@@ -41,29 +41,57 @@ library neorv32;
 entity neorv32_ProcessorTop_Minimal is
   generic (
     -- General --
-    CLOCK_FREQUENCY   : natural := 0;       -- clock frequency of clk_i in Hz
+    CLOCK_FREQUENCY   : natural := 10000000;-- clock frequency of clk_i in Hz
     -- Internal Instruction memory --
-    MEM_INT_IMEM_EN   : boolean := true;    -- implement processor-internal instruction memory
-    MEM_INT_IMEM_SIZE : natural := 8*1024;  -- size of processor-internal instruction memory in bytes
+    MEM_INT_IMEM_EN   : boolean := false;   -- implement processor-internal instruction memory
+    MEM_INT_IMEM_SIZE : natural := 0;  -- size of processor-internal instruction memory in bytes
     -- Internal Data memory --
     MEM_INT_DMEM_EN   : boolean := true;    -- implement processor-internal data memory
-    MEM_INT_DMEM_SIZE : natural := 64*1024; -- size of processor-internal data memory in bytes
+    MEM_INT_DMEM_SIZE : natural := 16*1024; -- size of processor-internal data memory in bytes
     -- Processor peripherals --
-    IO_PWM_NUM_CH     : natural := 3        -- number of PWM channels to implement (0..12); 0 = disabled
+    IO_PWM_NUM_CH     : natural := 0;       -- number of PWM channels to implement (0..12); 0 = disabled
+    -- External memory interface (WISHBONE) --
+    MEM_EXT_EN        : boolean := true;   -- implement external memory bus interface?
+    -- RISC-V CPU Extensions --
+    CPU_EXTENSION_RISCV_A        : boolean := false;  -- implement atomic memory operations extension?
+    CPU_EXTENSION_RISCV_B        : boolean := false;  -- implement bit-manipulation extension?
+    CPU_EXTENSION_RISCV_C        : boolean := false;  -- implement compressed extension?
+    CPU_EXTENSION_RISCV_E        : boolean := false;  -- implement embedded RF extension?
+    CPU_EXTENSION_RISCV_M        : boolean := false;  -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_U        : boolean := false;  -- implement user mode extension?
+    CPU_EXTENSION_RISCV_Zfinx    : boolean := false;  -- implement 32-bit floating-point extension (using INT regs!)
+    CPU_EXTENSION_RISCV_Zicntr   : boolean := true;   -- implement base counters?
+    CPU_EXTENSION_RISCV_Zihpm    : boolean := false;  -- implement hardware performance monitors?
+    CPU_EXTENSION_RISCV_Zifencei : boolean := false;  -- implement instruction stream sync.?
+    CPU_EXTENSION_RISCV_Zmmul    : boolean := false;  -- implement multiply-only M sub-extension?
+    CPU_EXTENSION_RISCV_Zxcfu    : boolean := false;  -- implement custom (instr.) functions unit?
+    -- Processor peripherals --
+    IO_GPIO_NUM       : natural range 0 to 64 := 8    -- number of GPIO input/output pairs (0..64)
   );
   port (
     -- Global control --
     clk_i  : in  std_logic;
     rstn_i : in  std_logic;
-    -- PWM (available if IO_PWM_NUM_CH > 0) --
-    pwm_o  : out std_ulogic_vector(IO_PWM_NUM_CH-1 downto 0)
+    -- Wishbone bus interface (available if MEM_EXT_EN = true) --
+    wb_tag_o  : out std_ulogic_vector(02 downto 0); -- request tag
+    wb_adr_o  : out std_ulogic_vector(31 downto 0); -- address
+    wb_dat_i  : in  std_ulogic_vector(31 downto 0) := (others => 'U'); -- read data
+    wb_dat_o  : out std_ulogic_vector(31 downto 0); -- write data
+    wb_we_o   : out std_ulogic; -- read/write
+    wb_sel_o  : out std_ulogic_vector(03 downto 0); -- byte enable
+    wb_stb_o  : out std_ulogic; -- strobe
+    wb_cyc_o  : out std_ulogic; -- valid cycle
+    wb_ack_i  : in  std_ulogic := 'L'; -- transfer acknowledge
+    wb_err_i  : in  std_ulogic := 'L'; -- transfer error
+    -- GPIO (available if IO_GPIO_NUM > 0) --
+    gpio_o    : out std_ulogic_vector(7 downto 0) -- parallel output
   );
 end entity;
 
 architecture neorv32_ProcessorTop_Minimal_rtl of neorv32_ProcessorTop_Minimal is
 
   -- internal IO connection --
-  signal con_pwm_o  : std_ulogic_vector(11 downto 0);
+  signal con_gpio_o  : std_ulogic_vector(63 downto 0);
 
 begin
 
@@ -82,18 +110,46 @@ begin
     MEM_INT_DMEM_SIZE            => MEM_INT_DMEM_SIZE, -- size of processor-internal data memory in bytes
     -- Processor peripherals --
     IO_MTIME_EN                  => true,              -- implement machine system timer (MTIME)?
-    IO_PWM_NUM_CH                => IO_PWM_NUM_CH      -- number of PWM channels to implement (0..12); 0 = disabled
+    IO_PWM_NUM_CH                => IO_PWM_NUM_CH,     -- number of PWM channels to implement (0..12); 0 = disabled
+    -- External memory interface (WISHBONE) --
+    MEM_EXT_EN                   => MEM_EXT_EN,        -- implement external memory bus interface?
+    -- RISC-V CPU Extensions --
+    CPU_EXTENSION_RISCV_A        => CPU_EXTENSION_RISCV_A,         -- implement atomic memory operations extension?
+    CPU_EXTENSION_RISCV_B        => CPU_EXTENSION_RISCV_B,         -- implement bit-manipulation extension?
+    CPU_EXTENSION_RISCV_C        => CPU_EXTENSION_RISCV_C,         -- implement compressed extension?
+    CPU_EXTENSION_RISCV_E        => CPU_EXTENSION_RISCV_E,         -- implement embedded RF extension?
+    CPU_EXTENSION_RISCV_M        => CPU_EXTENSION_RISCV_M,         -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_U        => CPU_EXTENSION_RISCV_U,         -- implement user mode extension?
+    CPU_EXTENSION_RISCV_Zfinx    => CPU_EXTENSION_RISCV_Zfinx,     -- implement 32-bit floating-point extension (using INT regs!)
+    CPU_EXTENSION_RISCV_Zicntr   => CPU_EXTENSION_RISCV_Zicntr,    -- implement base counters?
+    CPU_EXTENSION_RISCV_Zihpm    => CPU_EXTENSION_RISCV_Zihpm,     -- implement hardware performance monitors?
+    CPU_EXTENSION_RISCV_Zifencei => CPU_EXTENSION_RISCV_Zifencei,  -- implement instruction stream sync.?
+    CPU_EXTENSION_RISCV_Zmmul    => CPU_EXTENSION_RISCV_Zmmul,     -- implement multiply-only M sub-extension?
+    CPU_EXTENSION_RISCV_Zxcfu    => CPU_EXTENSION_RISCV_Zxcfu,     -- implement custom (instr.) functions unit?
+    -- Processor peripherals --
+    IO_GPIO_NUM                  => IO_GPIO_NUM         -- number of GPIO input/output pairs (0..64)
   )
   port map (
     -- Global control --
     clk_i  => clk_i,    -- global clock, rising edge
     rstn_i => rstn_i,   -- global reset, low-active, async
-    -- PWM (available if IO_PWM_NUM_CH > 0) --
-    pwm_o  => con_pwm_o -- pwm channels
+    -- GPIO (available if IO_GPIO_NUM > 0) --
+    gpio_o  => con_gpio_o, -- parallel output
+    -- Wishbone bus interface (available if MEM_EXT_EN = true) --
+    wb_tag_o => wb_tag_o, 
+    wb_adr_o => wb_adr_o,
+    wb_dat_i => wb_dat_i,
+    wb_dat_o => wb_dat_o,
+    wb_we_o  => wb_we_o,
+    wb_sel_o => wb_sel_o,
+    wb_stb_o => wb_stb_o,
+    wb_cyc_o => wb_cyc_o,
+    wb_ack_i => wb_ack_i,
+    wb_err_i => wb_err_i
   );
 
-  -- PWM --
-  pwm_o <= con_pwm_o(IO_PWM_NUM_CH-1 downto 0);
+  -- GPIO --
+  gpio_o <= con_gpio_o(7 downto 0);
 
 
 end architecture;
