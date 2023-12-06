ef5d4c185731ec86149ac03364e48350c5c9fdfa
edianannink <e_dian@live.nl>
2023-12-06 23:17:48 +0000

    app img date

--- rtl/core/neorv32_application_image.vhd
+++ rtl/core/neorv32_application_image.vhd
@@ -2,7 +2,7 @@
 -- Auto-generated memory initialization file (for APPLICATION) from source file <coremark/../../cores/neorv32/bin/riscv-target-firmware.bin>
 -- Size: 29884 bytes
 -- MARCH: default
--- Built: 06.12.2023 22:46:58
+-- Built: 06.12.2023 23:16:43
 
 -- prototype defined in 'neorv32_package.vhd'
 package body neorv32_application_image is
